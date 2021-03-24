444dd9634c27b2c83c802b1fcbdfd010d60c40d4
pkpatch <Kilmacno1@hotmail.com>
2021-03-24 16:23:21 +0000

    Initial commit - To be reviewed bt Astha

---
+++ src/com/sparta/patrick/Main.java
@@ -0,0 +1,12 @@
+package com.sparta.patrick;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = {6,7,8,9,2,7,20,26,84,16,05,985,1};
+
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.sort(numbers);
+
+    }
+}
---
+++ src/com/sparta/patrick/utils/Printer.java
@@ -0,0 +1,10 @@
+package com.sparta.patrick.utils;
+
+public class Printer {
+
+    public static void printArray(int[] numbers) {
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
